783c030838fb6ea5cf8b0eaa5abcf87731ae38d0
Mohammad Rizwan <rizwanm76@yahoo.com>
2024-02-02 13:44:48 -0500

    testing build of JWT token and decoding it for correct fields

--- src/main/java/com/rizzywebworks/InspireHub/controller/AuthController.java
+++ src/main/java/com/rizzywebworks/InspireHub/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.rizzywebworks.InspireHub.controller;
 
 import com.rizzywebworks.InspireHub.model.LoginRequest;
 import com.rizzywebworks.InspireHub.model.LoginResponse;
+import com.rizzywebworks.InspireHub.security.JwtIssuer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,14 +10,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final JwtIssuer jwtIssuer;
     @PostMapping("/auth/login")
     public LoginResponse login(@RequestBody @Validated LoginRequest request) {
+        var token = jwtIssuer.issue(1L, request.getEmail(), List.of("USER"));
         return LoginResponse.builder()
-                .accessToken("blah blah blah")
+                .accessToken(token)
                 .build();
     }
+    // This is for testing to make sure the JWT is being build correctly with the correct
+    // fields to identify the user
 }
