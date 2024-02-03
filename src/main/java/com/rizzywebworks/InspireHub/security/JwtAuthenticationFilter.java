2a87940acf6e2fd29b05f38d0bd064217c640f27
Mohammad Rizwan <rizwanm76@yahoo.com>
2024-02-03 00:43:24 -0500

    setup jwt convertor classes for auth

---
+++ src/main/java/com/rizzywebworks/InspireHub/security/JwtAuthenticationFilter.java
@@ -0,0 +1,23 @@
+package com.rizzywebworks.InspireHub.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtDecoder jwtDecoder;
+
+    private final JwtToPrincipalConverter jwtToPrincipalConverter;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        filterChain.doFilter(request, response);
+    }
+}
